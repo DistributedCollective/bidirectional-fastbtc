1f34ea6e07cafa73552e36f558c706b6738b04ec
Rainer Koirikivi <rainer@koirikivi.fi>
2021-11-02 03:17:04 +0200

    Wait for rsk startup

--- packages/fastbtc-node/startup.js
+++ packages/fastbtc-node/startup.js
@@ -1,6 +1,13 @@
 const child_process = require('child_process');
 const ormconfig = require('./ormconfig');
 
+
+const rskUrl = process.env.FASTBTC_RSK_RPC_URL;
+if (!rskUrl) {
+    throw new Error('required env var FASTBTC_RSK_RPC_URL not set!')
+}
+
+
 console.log('Waiting for postgresql startup at', ormconfig.url);
 let dbWaitSuccess = false;
 for(let attempt = 0; attempt < 100; attempt++) {
@@ -30,6 +37,34 @@ child_process.spawnSync(
     ['migration:run'],
     {stdio: 'inherit'}
 );
+
+
+console.log('Waiting for rsk network at', rskUrl);
+let rskWaitSuccess = false;
+for(let attempt = 0; attempt < 100; attempt++) {
+    const ret = child_process.spawnSync(
+        "curl",
+        [rskUrl],
+    );
+
+    if (ret.status === null && ret.error?.code === 'ENOENT') {
+        console.warn('curl not found. continuing anyway');
+        rskWaitSuccess = true;
+        break;
+    }
+    if (ret.status === 0) {
+        rskWaitSuccess = true;
+        break;
+    }
+    child_process.spawnSync(
+        "sleep",
+        ["2"],
+    );
+}
+if (!rskWaitSuccess) {
+    throw new Error('rsk wait failure');
+}
+
 
 const main = require('./dist/index.js')['default'];
 main();
