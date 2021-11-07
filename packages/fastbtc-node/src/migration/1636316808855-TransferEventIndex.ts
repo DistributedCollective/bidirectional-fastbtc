bf04c5c9888b73eed7e70e658c5b951b3ed7d09f
Rainer Koirikivi <rainer@koirikivi.fi>
2021-11-08 01:17:29 +0200

    Apparently you cannot just rename index in migration with TypeORM :P

--- packages/fastbtc-node/src/migration/1636316808855-TransferEventIndex.ts
+++ packages/fastbtc-node/src/migration/1636316808855-TransferEventIndex.ts
@@ -4,10 +4,10 @@ export class TransferEventIndex1636316808855 implements MigrationInterface {
     name = 'TransferEventIndex1636316808855'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_TransferEvent" ON "public"."transfer" ("rsk_block_number", "rsk_transaction_index", "rsk_log_index") `);
+        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_13e4021888aa27af3459659665" ON "public"."transfer" ("rsk_block_number", "rsk_transaction_index", "rsk_log_index") `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "public"."IDX_TransferEvent"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_13e4021888aa27af3459659665"`);
     }
 }
