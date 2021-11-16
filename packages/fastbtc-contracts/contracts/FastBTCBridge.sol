a56680a051f44b92838d9855306ad20f4531c167
Rainer Koirikivi <rainer@koirikivi.fi>
2021-11-16 14:06:33 +0200

    Remove unnecessary import

--- packages/fastbtc-contracts/contracts/FastBTCBridge.sol
+++ packages/fastbtc-contracts/contracts/FastBTCBridge.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "./Freezable.sol";
-import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
